87d7e9fc838f97a0c2feb33164dd21b079882122
folkens2 <tfolkens@chicagobooth.edu>
2017-04-25 16:04:40 -0500

    lion route fixes

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,10 @@ Rails.application.routes.draw do
   # Uncomment each route below ONE AT A TIME and debug.
 
   get("/zodiacs/aries",       { :controller =>  "zodiacs",  :action =>  "ram" })
-  # get("/zodiacs/taurus",      { :controller,   "zodiacs", :action,   "bull" })
-  # get(/zodiacs/gemini,        { :controller => "zodiacs", :action => "twins" })
-  # get("/zodiacs/cancer"       { :controller => "zodiacs"  :action => "crab" })
-  # get ("/zodiacs/leo",        { :controller => "zodiac",  :action => "lion" })
+  get("/zodiacs/taurus",      { :controller =>  "zodiacs",  :action =>  "bull" })
+  get("/zodiacs/gemini",        { :controller => "zodiacs", :action => "twins" })
+  get("/zodiacs/cancer",       { :controller => "zodiacs",  :action => "crab" })
+  get ("/zodiacs/leo",        { :controller => "zodiac",  :action => "lion" })
   # get(" /zodiacs/virgo",      { :controller => "Zodiacs", :action => "maiden" })
   # get("/zodiacs/libra",       { :controller => "zodiacs", :action => "scales" })
   # get("/zodiac/scorpio",      { :controller => "zodiacs", :action => "scorpion" })
